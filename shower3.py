10db53160e0edd0a994c2d2ccb7d9891d098e702
magnusiv <magnusiv@users.noreply.github.com>
2014-10-13 15:53:17 +0200

    Update shower3.py

--- shower3.py
+++ shower3.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 
 n      = 4
 omegas = [.01,.5,1.,5.]
+end = 7.
 
 ax1 = plt.subplot(1,1,1)
 ax1.set_title('Eigenfunctions')
-ax1.set_ylabel('u(rho)')
+ax1.set_ylabel('|R(rho)|^2')
 ax1.set_xlabel(  'rho' )
 ax1.grid(True)
 
@@ -16,9 +17,13 @@ for i in range(0,n):
     X = np.transpose(np.loadtxt(name))
     X = X[::-1]
     N = len(X)
-    RHO = np.linspace(0.,27.0,N+2)
-    XX = [0]; XX.extend(X); XX.append(0)
-    ax1.plot( RHO,XX, label='omega = %4.2f' % (omega[i]) )
+    RHO = np.linspace(end/(N+2),end*(1.-1./(N+2)),N)
+    for j in range(0,N):
+	X[j] = (X[j]/RHO[j])**2
+    X = X/np.linalg.norm(X)
+    RHO = np.linspace(end/(N+2),end,N+1)
+    XX = []; XX.extend(X); XX.append(0)
+    ax1.plot( RHO,XX, label='omega = %4.2f' % (omegas[i]) )
 
 ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
